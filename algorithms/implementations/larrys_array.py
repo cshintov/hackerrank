983dc521d92b918880fda06c2c8991b02046d50d
shinto cv <cshintov@gmail.com>
2016-04-22 19:12:01 +0530

    changes office

--- algorithms/implementations/larrys_array.py
+++ algorithms/implementations/larrys_array.py
@@ -48,31 +48,48 @@ class Sortable(Exception):
     """ Sortable exception """
     pass
 
+class NotSortable(Exception):
+    """ Not Sortable exception """
+    pass
+    
+
+
+from functools import partial
 from pdb import set_trace
-def gen_states(array):
+def _gen_states(array, starting_states):
     """ generate all possible states in the process of sorting """
-    set_trace()
     initial_states = gen_states_helper(array)
     for state in initial_states:
+        starting_states.append(state)
         temp = gen_states_helper(state)
         for tst in temp:
+            if tst in starting_states:
+                raise NotSortable
             if is_sorted(tst):
                 raise Sortable
             try:
-                gen_states(tst)
-            except RuntimeError:
+                starting_states.append(tst)
+                _gen_states(tst, starting_states)
+            except NotSortable:
                 continue
 
+    raise NotSortable
 
-def is_sortable(array):
+
+def is_sortable(array, parents=[]):
     """ checks whether the robot can sort the array """
-    try:
-        gen_states(array)
-    except Sortable:
-        return True
-    except RuntimeError:
-        return False
-
+    if is_sorted(array):
+        return 'YES'
+    children = gen_states_helper(array)
+    for child in children:
+        if is_sorted(child):
+            return 'YES'
+        if child in parents:
+            continue
+        parents.append(array)
+        return is_sortable(child, parents) 
+    return 'NO'
+    
 
 def solve():
     """ solve it """
@@ -84,4 +101,13 @@ def solve():
 
 
 if __name__ == '__main__':
-    solve()
+    #solve()
+    #is_sortable([3,1,2])
+    #raw_input()
+    #is_sortable([1,3,2])
+    #raw_input()
+    #is_sortable([1,3, 4, 2])
+    #raw_input()
+    #is_sortable([1,2,3,5,4])
+    set_trace()
+    print is_sortable([1,6,4,2,3,5,3,6,4])
